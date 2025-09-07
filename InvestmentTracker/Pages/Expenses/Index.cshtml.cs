f903c96aeda604318f8f4c1a70e3323782358b68
Michal Zezula <zezula.mi@gmail.com>
2025-09-07 14:21:30 +0200

    initial expense tracker page init

---
+++ InvestmentTracker/Pages/Expenses/Index.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using InvestmentTracker.Services;
+using InvestmentTracker.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InvestmentTracker.Pages.Expenses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IExpenseService _expenseService;
+
+        public IndexModel(IExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public int? Year { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? Month { get; set; }
+
+        public MonthlyExpenseViewModel ViewModel { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            var selectedYear = Year ?? DateTime.Today.Year;
+            var selectedMonth = Month ?? DateTime.Today.Month;
+
+            ViewModel = await _expenseService.GetMonthlyDataAsync(selectedYear, selectedMonth);
+        }
+    }
+}
